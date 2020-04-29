f80a75c1e65e8a92d9fdd568002d3943d8c5802f
plane <peter.lane@ama-assn.org>
2020-04-29 17:04:14 -0500

    Unix line endings.

--- Go/src/muffinspawn/environment/start.go
+++ Go/src/muffinspawn/environment/start.go
@@ -21,6 +21,7 @@ func StartVirtualEnvironment(path string) {
 
 	if runtime.GOOS == "windows" {
 		command := exec.Command(shell_path, "-NoExit", command_path)
+		// command := exec.Command(shell_path, "-NoExit", command_path , ";", "python --version")
 
 		command.Env = os.Environ()
 		command.Stdout = os.Stdout
@@ -30,7 +31,6 @@ func StartVirtualEnvironment(path string) {
 		if err := command.Run(); err != nil {
 			panic(err)
 		}
-
 	} else {
 		if err := syscall.Exec(shell_path, []string{command_path}, os.Environ()); err != nil {
 			panic(err)
@@ -51,6 +51,7 @@ func os_specific_command_paths(base_path string) (shell_path, command_path strin
 
 
 func windows_command_paths(base_path string) (shell_path, command_path string, err error) {
+	// Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 	shell_path, err = exec.LookPath("PowerShell.exe")
 	command_path = base_path + string(os.PathSeparator) + "Scripts" + string(os.PathSeparator) + "Activate.ps1"
 
